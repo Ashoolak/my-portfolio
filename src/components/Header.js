f5d24b6053e8fd147fa59939d01741d208b68c25
Ashkan Yazdi Zadeh <55773710+Ashoolak@users.noreply.github.com>
2024-02-08 17:05:07 -0800

    Fixed the expanded menu not closing

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,11 @@ export default function Header() {
 
   const toggleMobileMenu = () => setIsMobileMenuOpen(!isMobileMenuOpen);
 
+  // Define a new function that both toggles the menu and allows for navigation
+  const handleLinkClick = () => {
+    setIsMobileMenuOpen(false); // This ensures the menu closes upon clicking a link
+  };
+
   return (
     <header className={styles.header}>
       <a className={styles.brandName} href="/#About">
@@ -20,15 +25,26 @@ export default function Header() {
         <FontAwesomeIcon icon={isMobileMenuOpen ? faTimes : faBars} size="lg" />
       </button>
       <nav className={isMobileMenuOpen ? styles.mobileNav : styles.desktopNav}>
-        <a href="/#Work-Experience">Work Experience</a>
-        <a href="/#Projects">Projects</a>
-        <a href="../Resume.pdf" target="_blank" rel="noopener noreferrer">
+        {/* Add onClick={handleLinkClick} to each link */}
+        <a href="/#Work-Experience" onClick={handleLinkClick}>
+          Work Experience
+        </a>
+        <a href="/#Projects" onClick={handleLinkClick}>
+          Projects
+        </a>
+        <a
+          href="../Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={handleLinkClick}
+        >
           Resume
         </a>
         <a
           href="https://www.linkedin.com/in/ashkan-yazdi-zadeh"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={handleLinkClick}
         >
           <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
@@ -36,10 +52,11 @@ export default function Header() {
           href="https://github.com/Ashoolak"
           target="_blank"
           rel="noopener noreferrer"
+          onClick={handleLinkClick}
         >
           <FontAwesomeIcon icon={faGithub} size="2x" />
         </a>
-        <a href="mailto:ashkanyz70@gmail.com">
+        <a href="mailto:ashkanyz70@gmail.com" onClick={handleLinkClick}>
           <FontAwesomeIcon icon={faEnvelope} size="2x" />
         </a>
       </nav>
