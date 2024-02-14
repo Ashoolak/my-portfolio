75cc8c6d2979b52bb5d9cfd6a38416b192a193aa
Ashkan Yazdi Zadeh <55773710+Ashoolak@users.noreply.github.com>
2024-02-13 22:24:13 -0800

    Fixed error in styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -32,10 +32,14 @@ export default function Header() {
       </button>
       <nav className={isMobileMenuOpen ? styles.mobileNav : styles.desktopNav}>
         {/* Add onClick={handleLinkClick} to each link */}
-        <a href="/#Work-Experience" onClick={handleLinkClick} className="2xl">
+        <a
+          href="/#Work-Experience"
+          onClick={handleLinkClick}
+          className="text-xl"
+        >
           Work Experience
         </a>
-        <a href="/#Projects" onClick={handleLinkClick} className="2xl">
+        <a href="/#Projects" onClick={handleLinkClick} className="text-xl">
           Projects
         </a>
         <a
@@ -43,7 +47,7 @@ export default function Header() {
           target="_blank"
           rel="noopener noreferrer"
           onClick={handleLinkClick}
-          className="2xl"
+          className="text-xl"
         >
           Resume
         </a>
